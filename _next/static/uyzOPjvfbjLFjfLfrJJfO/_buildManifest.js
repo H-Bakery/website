4f886d5103baea3262f702ef4a09d00e3d191dae
huhn511 <huhn511@users.noreply.github.com>
2022-07-16 16:56:00 +0000

    deploy: 4a67ddcd7bee3ce2a2b9c5c9511f7dfe499ba34a

--- _next/static/uyzOPjvfbjLFjfLfrJJfO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/174-ef743d9aba77f108cbbb.js","static/chunks/pages/index-35a8c541410c5465f1e9.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
