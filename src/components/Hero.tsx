4a67ddcd7bee3ce2a2b9c5c9511f7dfe499ba34a
huhn511 <sehe89@gmail.com>
2022-07-16 18:54:48 +0200

    add hero bg video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { Background } from './Background';
-import { Button } from './Button';
-import { HeroOneButton } from './HeroOneButton';
-import { Section } from './Section';
-import { NavbarTwoColumns } from './NavbarTwoColumns';
-import { Logo } from './Logo';
+import { Background } from "./Background";
+import { Button } from "./Button";
+import { HeroOneButton } from "./HeroOneButton";
+import { Section } from "./Section";
+import { NavbarTwoColumns } from "./NavbarTwoColumns";
+import { Logo } from "./Logo";
 
 const Hero = () => (
   <Background color="bg-gray-100">
@@ -27,14 +27,21 @@ const Hero = () => (
           </Link>
         </li>
       </NavbarTwoColumns>
-    </Section>
+    </Section>    
 
     <Section yPadding="pt-20 pb-32">
+    <video id="background-video" autoPlay muted loop>
+      <source src="/assets/images/stock/bg_video.mp4" type="video/mp4" />
+      <source src="/assets/images/stock/bg_video.mp4" type="video/ogg" />
+      Your browser does not support the video tag.
+    </video>
       <HeroOneButton
         title={
           <>
-            {"Let's taste "}
-            <span className="text-primary-500">Perfection</span>
+            {"täglich "}
+            <span className="text-primary-500">frisch </span>
+            {"aus "}
+            <span className="text-primary-500">Meisterhand </span>
           </>
         }
         description="The easiest way to build a React landing page in seconds."
