6bb4330756f5efb3f77ab9bbbc346bcc1a311397
huhn511 <sehe89@gmail.com>
2022-07-27 16:41:17 +0200

    add products data

---
+++ src/mocks/products/index.ts
@@ -0,0 +1,17 @@
+const BREADS = require("./breads.json");
+const BAGUETTES = require("./baguettes.json");
+const BUNS = require("./buns.json");
+const SWEETS = require("./sweets.json");
+const CAKES = require("./cakes.json");
+const SPECIAL_CAKES = require("./special-cakes.json");
+const SNACKS = require("./snacks.json");
+
+export const PRODUCTS = [
+  ...BREADS,
+  ...BAGUETTES,
+  ...BUNS,
+  ...SWEETS,
+  ...CAKES,
+  ...SPECIAL_CAKES,
+  ...SNACKS,
+];
