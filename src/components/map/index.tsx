4cb392e542914b471c67b1e3d9b09729ddefd0d9
huhn511 <sehe89@gmail.com>
2022-07-28 01:23:37 +0200

    add google maps

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,20 +1,37 @@
-import { Box, Typography } from '@mui/material'
+import { Box } from '@mui/material'
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }: any) => <h1>{text}</h1>;
+
 
 const Map: React.FC = () => {
+  const defaultProps = {
+    // 49.30107377123533, 7.369370264295438
+    center: {
+      lat: 49.30107377123533,
+      lng: 7.369370264295438
+    },
+    zoom: 18
+  };
+
   return (
-    <Box>
-      <Box sx={styles.map}>
-        Map Goes here
-      </Box>
-      <Box sx={styles.info}>
-        <Typography>Öffnungszeiten</Typography>
-        <Box>
-          
-        </Box>
-      </Box>
-    </Box>
-  )
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAqcU5e900koplaN_FRB_b8v2Iw44KoK4s" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+        // 49.301429495245586, 7.369493502873482
+          lat={49.301429495245586}
+          lng={7.369493502873482}
+          text="Bäckerei Heusser"
+        />
+      </GoogleMapReact>
+    </div>
+  );
 }
 
 const styles = {
