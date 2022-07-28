88d7b3e857e911ba0028b406b88184d09878a9cc
huhn511 <sehe89@gmail.com>
2022-07-28 02:46:32 +0200

    add news and kundenbwertung mocks

---
+++ src/mocks/kundenbewertungen/index.ts
@@ -0,0 +1,15 @@
+export const KUNDENBEWERTUNGEN = [
+    {
+      name: "Freyja7",
+      text: `das ist noch eine 3 Generationen - Bäckerei da wird noch alles, Brot Brötchen, Kuchen, Torten selber gebacken nix vom Band. Echte Handwerkskunst vom Feinsten 👌🏻😍`,
+    },
+    {
+      name: "Sarah K",
+      text: `Lieblings Bäckerei! Seit der Kindheit!!! Hier wird noch selbst gebacken und es schmeckt ausgezeichnet! Preis Leistung top👌 Dazu ein außergewöhnlich freundliches Personal!!`,
+    },
+    {
+      name: "Niko H",
+      text: `Super leckere Brötchen. Schön groß, einfach toll`,
+    },
+  ];
+  
---
+++ src/mocks/news/index.ts
@@ -0,0 +1,36 @@
+export const NEWS = [
+  {
+    name: "Verkaufspartner gesucht!",
+    published: "25.07.2022",
+    category: "Internes",
+    image: "",
+    text: `Wir suchen in der Vorstadt Homburg, Bereich Birkensiedlung-Realschule-Saarpfalz-Gymnasium- Waldstadion Jemanden, der in Eigenverantwortung und mit eigenem Verkaufsplatz bereit wäre, unsere Backwaren an unseren Öffnungstagen Mo, Di, Do, Fr und Sa zu verkaufen. Bitte kontaktieren Sie uns hierzu über die angegebenen Kontaktmöglichkeiten. Alles Weitere können wir in einen persönlichen Gespräch besprechen.`,
+  },
+  {
+    name: "Geänderte Öffnungszeiten in den Sommerferien!",
+    published: "25.07.2022",
+    category: "Internes",
+    image: "",
+    text: `Sehr geehrte Kunden, bitte beachten Sie unsere geänderten Öffnungszeiten in den Ferien: Montag, Dienstag, Donnerstag und Freitag von 5 bis 11 Uhr und von 15:30 bis 18 Uhr. Samstag von 6 bis 13 Uhr und Mittwoch und Sonntags sind wie gehabt Ruhetage.`,
+  },
+  {
+    name: "Neue Abholstation!",
+    published: "24.07.2022",
+    category: "Internes",
+    image: "",
+    text: `Unsere Abholstation zum Abholen von vorbestellten Backwaren in der Mittagspause oder nach Feierabend.`,
+  },
+  {
+    name: "Aushilfe gesucht!",
+    published: "15.06.2022",
+    category: "Internes",
+    image: "",
+    text: `Wir suchen eine Aushilfe im Backbetrieb, möglichst dauerhaft bei einer 5-Tage-Woche.
+    Arbeitszeiten der 5-Tage Woche (35 Stunden Höchstwert) sind:
+    Mo, Di, Do, Fr: 3:00 - 8:00 Uhr (ca.)
+    Sa: 0:00 - 5:00 Uhr (ca.)
+    Mittwoch und Sonn- und Feiertage frei
+    Abweichende Feiertagsregelung
+    Vergütung: 60€/Schicht VB`,
+  },
+];
