eb8587e34c6759c11d29c54a24bb4cd03dbe1414
Sven Reiser <svreiser@gmail.com>
2022-08-10 21:18:48 +0200

    adds filters

--- src/components/sortiment/Filter.tsx
+++ src/components/sortiment/Filter.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import { PRODUCTS } from '../../mocks/products'
+import Brot from '../icons/products/Brot'
+import Broetchen from '../icons/products/Broetchen'
+import Teilchen from '../icons/products/Teilchen'
+import Kuchen from '../icons/products/Kuchen'
+import Torten from '../icons/products/Torten'
+import Getranke from '../icons/products/Getranke'
 
 interface Item {
 	id: number
@@ -14,9 +20,23 @@ interface Props {
 	setProducts: (items: Item[]) => void
 }
 
+const FILTERS = [
+  { label: 'Brot', icon: <Brot /> },
+  { label: 'Brötchen',  icon: <Broetchen /> },
+  { label: 'Teilchen',  icon: <Teilchen /> },
+  { label: 'Kuchen',  icon: <Kuchen /> },
+  { label: 'Torten',  icon: <Torten /> },
+  { label: 'Getränke',  icon: <Getranke /> },
+]
+
 const Filter: React.FC<Props> = (props) => {
 	const { setProducts } = props
+  const [selected, setSelected] = React.useState('')
 
+  React.useEffect(() => {
+	  filter("Brot")
+  }, [])
+  
 	const filter = (input: String) => {
     const newArray = 
       PRODUCTS.filter((product) =>
@@ -25,19 +45,53 @@ const Filter: React.FC<Props> = (props) => {
       .map((filteredName) => filteredName)
 
     setProducts(newArray)
+    setSelected(input as string)
   }
 
+  console.log(selected)
+
   return (
-    <Box>
-			<button onClick={() => filter("Brot")}>Brote</button>
-			<button onClick={() => filter("Kaffeestückchen")}>
-				Kaffeestückchen
-			</button>
-			<button onClick={() => filter("Snacks")}>Snacks</button>
-			<button onClick={() => filter("Kuchen")}>Kuchen</button>
-			<button onClick={() => filter("Torten")}>Torten</button>
+    <Box sx={styles.root}>
+      <Grid container spacing={4}>
+        {FILTERS.map((item) => (
+          <Grid item xs={2}>
+            <Box
+              sx={styles.item}
+              onClick={() => filter(item.label)}
+              className={`${item.label === selected ? 'active' : ''}`}
+            >
+              {item.icon}
+              <Typography mt={1}>{item.label}</Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   )
+}
+
+const styles = {
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    mb: 4
+  },
+  item: {
+    bgcolor: 'background.paper',
+    borderRadius: '8px',
+    boxShadow: 1,
+    p: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0.66,
+    transition: 'opacity ease-in-out 200ms',
+    
+    '&.active': {
+      opacity: 1,
+    }
+  }
 }
 
 export default Filter
