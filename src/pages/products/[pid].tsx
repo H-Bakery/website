6d55e62272de5ed248c47bc14d7d9c86130ebcfa
huhn511 <sehe89@gmail.com>
2022-08-31 22:57:46 +0200

    fix products reload

--- src/pages/products/[pid].tsx
+++ src/pages/products/[pid].tsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Box, Chip, Container, Grid, Typography } from '@mui/material'
+import React from "react";
+import { Box, Chip, Container, Grid, Typography } from "@mui/material";
 
-import Base from '../../layout/Base'
-import Hero from '../../components/Hero'
-import { useRouter } from 'next/router'
-import { PRODUCTS } from '../../mocks/products'
-import { Product } from '../../components/products/types'
-import { formatter } from '../../utils/formatPrice'
-import Button from '../../components/button/Index'
-import { CartContext } from '../../context/CartContext'
+import Base from "../../layout/Base";
+import Hero from "../../components/Hero";
+import { useRouter } from "next/router";
+import { PRODUCTS } from "../../mocks/products";
+import { Product } from "../../components/products/types";
+import { formatter } from "../../utils/formatPrice";
+import Button from "../../components/button/Index";
+import { CartContext } from "../../context/CartContext";
 
-const Index: React.FC = () => { 
-  const router = useRouter()
-  const { pid } = router.query
+const Index: React.FC = () => {
+  const router = useRouter();
+  const { pid } = router.query;
 
-  const { add } = React.useContext(CartContext)
+  const { add } = React.useContext(CartContext);
 
-  const products: Product[] = PRODUCTS.filter((item) => Number(pid) === Number(item.id))
-  const currentProduct: Product = products[0]
+  const products: Product[] = PRODUCTS.filter(
+    (item) => Number(pid) === Number(item.id)
+  );
+  const currentProduct: Product = products[0];
 
-  return (  
+  return (
     <Base>
-      <Container maxWidth='sm'>
+      <Container maxWidth="sm">
         <Hero title={currentProduct?.name} />
         <Grid container spacing={2}>
           <Grid item xs={6}>
@@ -32,8 +34,8 @@ const Index: React.FC = () => {
           <Grid item xs={6}>
             <Typography>{currentProduct?.name} Beschreibung</Typography>
             <Box sx={styles.footer}>
-              <Chip size='small' label={currentProduct?.category} />
-              <Typography variant='button' fontSize='16px'>
+              <Chip size="small" label={currentProduct?.category} />
+              <Typography variant="button" fontSize="16px">
                 {formatter.format(currentProduct?.price)}
               </Typography>
             </Box>
@@ -44,33 +46,59 @@ const Index: React.FC = () => {
         </Grid>
       </Container>
     </Base>
-  )
-}
+  );
+};
 
 const styles = {
   image: {
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: { xs: '45vw', sm: '260px'},
-    width: '100%',
-    borderRadius: '8px',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: { xs: "45vw", sm: "260px" },
+    width: "100%",
+    borderRadius: "8px",
     boxShadow: 1,
     mb: 2,
-    bgcolor: 'background.paper',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    bgcolor: "background.paper",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
 
-    '& img': {
-      maxWidth: '80%'
-    }
+    "& img": {
+      maxWidth: "80%",
+    },
   },
   footer: {
     mt: 2,
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  }
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+};
+
+export async function getStaticProps({ ...ctx }) {
+  const { pid } = ctx.params;
+
+  console.log("products pid", pid);
+  return {
+    props: {
+      pid
+    },
+  };
+}
+export async function getStaticPaths() {
+  // const files = fs.readdirSync('projects')
+  // const news: NewsType[] = NEWS.filter((item) => slug === item.slug)
+
+  const paths = PRODUCTS.map((item) => ({
+    params: {
+      pid: item.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export default Index
+export default Index;
