dc1f1f18b16cc5baee8acd07d460653a921d2d40
huhn511 <sehe89@gmail.com>
2023-10-31 02:34:48 +0100

    edit open hours in footer

--- src/components/footer/Openings.tsx
+++ src/components/footer/Openings.tsx
@@ -4,33 +4,41 @@ import React from 'react'
 const Openings: React.FC = () => {
   return (
     <Box>
-			<Typography variant='h6' fontSize={16} gutterBottom>
-				Öffnungszeiten
-			</Typography>
-			<Box sx={{mb: 2}}>
-				<Typography variant='body2' color='text.disabled'>
-					Mo, Di, Do, Fr
-				</Typography>
-				<Typography variant='body2' color='text.secondary'>
-					05:00 - 12:00 und 14:00 - 17:00 Uhr
-				</Typography>
-			</Box>
-			<Box sx={{mb: 2}}>
-				<Typography variant='body2' color='text.disabled'>
-					Sa
-				</Typography>
-				<Typography variant='body2' color='text.secondary'>
-					05:00 - 13:00 Uhr
-				</Typography>
-			</Box>
-			<Box sx={{mb: 2}}>
-				<Typography variant='body2' color='text.disabled'>
-					Mi, So
-				</Typography>
-				<Typography variant='body2' color='text.secondary'>
-					Ruhetag
-				</Typography>
-			</Box>
+      <Typography variant="h6" fontSize={16} gutterBottom>
+        Öffnungszeiten
+      </Typography>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body2" color="text.disabled">
+          Di, Do, Fr
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          06:00 - 15:00 Uhr
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body2" color="text.disabled">
+          Sa
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          06:00 - 13:00 Uhr
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body2" color="text.disabled">
+          So und Feiertage
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          08:00 - 11:00 Uhr
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body2" color="text.disabled">
+          Mo
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Ruhetag
+        </Typography>
+      </Box>
     </Box>
   )
 }
